9db5fb2bcfd8ab7f14237f696a854d5b4464a173
folkens2 <tfolkens@chicagobooth.edu>
2017-05-04 14:13:31 -0500

    update show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,35 +6,36 @@ class MoviesController < ApplicationController
     render("movies/index.html.erb")
   end
 
-
   def new_form
     render("movies/new_form.html.erb")
   end
 
-
   def create_row
-    #code
-  end
+    @movie = Movie.new
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
 
+    @movie.save
+    render("movies/show_details.html.erb")
+  end
 
   def show
     #code
   end
 
-
   def edit_form
     #code
   end
-
 
   def update_row
     #code
   end
 
-
   def destroy
     #code
   end
-
 
 end

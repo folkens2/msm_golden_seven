799ef260ac3f53fd89e93939286c73e8d4cb78f0
folkens2 <tfolkens@chicagobooth.edu>
2017-05-04 10:44:28 -0500

    removed all the spaces between get ()

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,39 @@
+class MoviesController < ApplicationController
+
+
+  def index
+    #code
+  end
+
+
+  def new_form
+    #code
+  end
+
+
+  def create_row
+    #code
+  end
+
+
+  def show
+    #code
+  end
+
+
+  def edit_form
+    #code
+  end
+
+
+  def update_row
+    #code
+  end
+
+
+  def destroy
+    #code
+  end
+
+
+end

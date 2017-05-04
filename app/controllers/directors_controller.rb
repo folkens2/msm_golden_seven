181a63054a091ee798998ca5b3c7de3c3045fe18
folkens2 <tfolkens@chicagobooth.edu>
2017-05-04 15:01:13 -0500

    director index page

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,53 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @directors = Director.all
+    render("directors/index.html.erb")
+  end
+
+  def new_form
+    render("directors/new_form.html.erb")
+  end
+
+  def create_row
+    @director = Director.new
+    @director.title = params[:title]
+    @director.year = params[:year]
+    @director.duration = params[:duration]
+    @director.description = params[:description]
+    @director.image_url = params[:image_url]
+
+    @director.save
+    render("directors/show_details.html.erb")
+  end
+
+  def show
+    @director = Director.find(params[:id])
+    render("directors/show_details.html.erb")
+  end
+
+  def edit_form
+    @director = Director.find(params[:id])
+    render("directors/edit_form.html.erb")
+  end
+
+  def update_row
+    @director = Director.find(params[:id])
+    @director.title = params[:title]
+    @director.year = params[:year]
+    @director.duration = params[:duration]
+    @director.description = params[:description]
+    @director.image_url = params[:image_url]
+
+    @director.save
+
+    render("directors/show_details.html.erb")
+  end
+
+  def destroy
+    @director = Director.find(params[:id])
+    @director.destroy
+    redirect_to("/")
+  end
+
+end
